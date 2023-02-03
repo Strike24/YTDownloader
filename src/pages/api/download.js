d5f4012bb2e91e472d29d8f7c5252ee58d7c38e5
Strike24 <benxcohen@gmail.com>
2023-02-03 18:51:13 +0200

    First Commit

---
+++ src/pages/api/download.js
@@ -0,0 +1,23 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+const fs = require('fs');
+const ytdl = require('ytdl-core');
+export default async function handler(req, res) {
+    //get the url from the request body
+    const url = req.query.link
+    console.log(url)
+    try {
+        let info = await ytdl.getBasicInfo(url);
+        res.setHeader('Content-disposition', `attachment; filename=${info.videoDetails.title}.mp3`);
+        ytdl(url, { filter: "audioonly" })
+            //download the file to the client side
+            .pipe(res);
+
+        console.log("passed")
+    } catch (error) {
+        res.status(500).send("Error, Video not found. Please try again with the right link")
+    }
+
+
+
+
+}
